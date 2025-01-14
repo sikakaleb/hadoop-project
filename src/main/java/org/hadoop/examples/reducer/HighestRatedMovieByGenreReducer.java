c3bd96c70703fe1d22f0b0848d82d0c490f98e0e
sikakaleb <dassou-ablam-kaleb.sika@etu.univ-cotedazur.fr>
2025-01-14 13:47:10 +0100

    ok

---
+++ src/main/java/org/hadoop/examples/reducer/HighestRatedMovieByGenreReducer.java
@@ -0,0 +1,38 @@
+package org.hadoop.examples.reducer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighestRatedMovieByGenreReducer extends Reducer<Text, Text, Text, Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Map<String, Float> movieRatings = new HashMap<>();
+        String genre = key.toString();
+
+        for (Text value : values) {
+            String val = value.toString();
+            if (val.startsWith("RATING:")) {
+                String[] parts = val.substring(7).split(":");
+                movieRatings.put(parts[0], Float.parseFloat(parts[1]));
+            }
+        }
+
+        String highestRatedMovie = null;
+        float highestRating = 0;
+
+        for (Map.Entry<String, Float> entry : movieRatings.entrySet()) {
+            if (entry.getValue() > highestRating) {
+                highestRating = entry.getValue();
+                highestRatedMovie = entry.getKey();
+            }
+        }
+
+        if (highestRatedMovie != null) {
+            context.write(new Text(genre), new Text(highestRatedMovie + " (Rating: " + highestRating + ")"));
+        }
+    }
+}
