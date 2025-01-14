b66cbe56e1bae67f4396fa0a2c0a19bc43bd3cd6
sikakaleb <dassou-ablam-kaleb.sika@etu.univ-cotedazur.fr>
2025-01-14 09:43:43 +0100

    last part

---
+++ src/main/java/org/hadoop/examples/CountMoviesByGenreMapper.java
@@ -0,0 +1,23 @@
+package org.hadoop.examples;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class CountMoviesByGenreMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text genre = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] columns = value.toString().split(",");
+        if (columns.length < 3) return; // Skip malformed lines
+        String[] genres = columns[2].split("\\|");
+        for (String g : genres) {
+            genre.set(g.trim());
+            context.write(genre, one);
+        }
+    }
+}
